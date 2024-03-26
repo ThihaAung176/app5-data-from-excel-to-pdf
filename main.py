813e5d1971d6716d9a114e88e1f86b7c2b5ce581
Sudo <thihhaaung.pol3@gmail.com>
2024-03-26 21:37:00 +0630

    pdf file for each excel

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import pandas as pd
 import glob
+from pathlib import Path
+from fpdf import FPDF
 
 filepaths = glob.glob("Invoices/*.xlsx")
 
 for filepath in filepaths:
     df = pd.read_excel(filepath, sheet_name="Sheet 1")
-    print(df)
+    pdf = FPDF(orientation="P", unit="mm", format="A4")
+    pdf.add_page()
+    pdf.set_font(family="times", style="B",size=16)
+    filename = Path(filepath).stem
+    invoice_nr = filename.split("-")[0]
+    pdf.cell(w=50,h=8, txt=f"Invoice nr.{invoice_nr}")
+    pdf.output(f"PDFS/{filename}.pdf")
