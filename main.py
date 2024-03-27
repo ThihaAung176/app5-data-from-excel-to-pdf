76af393daf929901ecb1dc5bd66e23af315688c9
Sudo <thihhaaung.pol3@gmail.com>
2024-03-27 10:47:21 +0630

    add date

--- main.py
+++ main.py
@@ -9,8 +9,14 @@ for filepath in filepaths:
     df = pd.read_excel(filepath, sheet_name="Sheet 1")
     pdf = FPDF(orientation="P", unit="mm", format="A4")
     pdf.add_page()
-    pdf.set_font(family="times", style="B",size=16)
+   
+    
     filename = Path(filepath).stem
-    invoice_nr = filename.split("-")[0]
-    pdf.cell(w=50,h=8, txt=f"Invoice nr.{invoice_nr}")
+    invoice_nr, date = filename.split("-")
+    
+    pdf.set_font(family="times", style="B",size=16)
+    pdf.cell(w=50,h=8, txt=f"Invoice nr.{invoice_nr}", ln=1)
+    
+    pdf.set_font(family="times", style="B",size=16)
+    pdf.cell(w=50,h=8, txt=f"Date: {date}")
     pdf.output(f"PDFS/{filename}.pdf")
